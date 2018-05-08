824d00f982b90e6857d68a204be0f73ec0bdedc7
Benjamin <benja.mora.torres@gmail.com>
2018-05-08 16:50:24 -0300

    El nuevo inicio

---
+++ Pasteleria/BD/Conexion.php
@@ -0,0 +1,14 @@
+<?php
+    class BD{
+        private $enlace;
+        private $stHost='localhost';
+        private $stUsuario='root'; 
+        private $stClave='';
+        private $stBd='pasteleria';
+
+        public function BD()
+        {
+            $this->enlace = new PDO("mysql:host=" . $this->stHost . ";dbname=" .$this->stBd ,$this->stUsuario ,$this->stClave);
+        }
+    }
+?> 
---
+++ Pasteleria/controladores/login.php
@@ -0,0 +1,16 @@
+<?php
+    $usuario = $_POST['usuario'];
+    $pass = $_POST['pass'];
+
+    if($usuario=="vendedor"){
+        header('Location: ../local/index.html');
+        
+    }    
+    if($usuario=="2"){
+        echo "Hola Vendedor";
+    }    
+    if($usuario=="cajero"){
+        header('Location: ../admin/index.html');
+    }
+
+?>
